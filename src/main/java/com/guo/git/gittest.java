ddd7c71a37c86cc7db6761642a28eaa261fed9e6
guo <1505426845@qq.com>
2023-09-07 09:15:23 +0800

    second commit

--- src/main/java/com/guo/git/gittest.java
+++ src/main/java/com/guo/git/gittest.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
